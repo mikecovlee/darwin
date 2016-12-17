b86356388d56cb3d0cb5aa10e1bf07feb0485530
Mike Lee <mikecovlee@163.com>
2016-12-17 19:16:03 +0800

    Signed-off-by: Mike Lee <mikecovlee@163.com>

--- headers/core.hpp
+++ headers/core.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <array>
+#include <cmath>
 #include <string>
 #include <memory>
 #include <typeinfo>
@@ -107,6 +108,16 @@ namespace darwin {
 		virtual std::size_t get_height() const=0;
 		virtual const pixel& get_pixel(const std::array<std::size_t,2>&) const=0;
 		virtual void draw_pixel(const std::array<std::size_t,2>&,const pixel&)=0;
+		virtual void draw_line(const std::array<std::size_t,2>& p0,const std::array<std::size_t,2>& p1,const pixel& pix)
+		{
+			if(p0==p1) return;
+			if(p0[0]>this->get_width()-1||p0[1]>this->get_height()-1||p1[0]>this->get_width()-1||p1[1]>this->get_height()-1)
+				throw std::out_of_range(__func__);
+			long w(p0[0]-p1[0]),h(p0[1]-p1[1]);
+			double distance(std::sqrt(std::pow(w,2)+std::pow(h,2)));
+			for(double c=0;c<=1;c+=1.0/distance)
+				this->draw_pixel({p0[0]+w*c,p0[1]+h*c},pix);
+		}
 		virtual void draw_picture(const std::array<std::size_t,2>& posit,const drawable& img)
 		{
 			std::size_t col(posit[0]),row(posit[1]);
