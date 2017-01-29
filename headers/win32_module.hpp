c20c844d307fb7d8070a02a40b24702f8ee2a82f
Michael Lee <mikecovlee@163.com>
2017-01-29 14:16:01 +0800

    增加Darwin对Windows的支持

--- headers/win32_module.hpp
+++ headers/win32_module.hpp
@@ -1,13 +1,30 @@
 #pragma once
+#include "../sources/win32_adapter.cpp"
 namespace darwin {
 	class win32_module_adapter:public module_adapter {
+		bool mReady=false;
 	public:
-		win32_module_adapter();
-		virtual ~win32_module_adapter();
-		virtual status get_state() const noexcept override;
-		virtual results load_module(const std::string&) noexcept override;
-		virtual results free_module() noexcept override;
-		virtual platform_adapter* get_platform_adapter() noexcept override;
+		win32_module_adapter() {}
+		virtual ~win32_module_adapter() {}
+		virtual status get_state() const noexcept override {
+			if(mReady)
+				return status::ready;
+			else
+				return status::leisure;
+		}
+		virtual results load_module(const std::string&) noexcept override {
+			mReady=true;
+			dwin32adapter.init();
+			return results::success;
+		}
+		virtual results free_module() noexcept override {
+			mReady=false;
+			dwin32adapter.stop();
+			return results::success;
+		}
+		virtual platform_adapter* get_platform_adapter() noexcept override {
+			return &dwin32adapter;
+		}
 	} dwin32module;
 	static darwin_rt runtime(&dwin32module);
 }
