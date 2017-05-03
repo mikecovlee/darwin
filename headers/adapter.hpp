85515e54276597fb9174839204d2c9e458214a2e
Michael Lee <mikecovlee@163.com>
2017-05-03 20:45:35 +0800

    格式化代码

--- headers/adapter.hpp
+++ headers/adapter.hpp
@@ -8,11 +8,11 @@ namespace darwin {
 		virtual ~platform_adapter()=default;
 		virtual void init()=0;
 		virtual void stop()=0;
-        virtual bool is_kb_hit() const=0;
-        virtual int get_kb_hit() const=0;
-        virtual void update(const canvas&)=0;
+		virtual bool is_kb_hit() const=0;
+		virtual int get_kb_hit() const=0;
+		virtual void update(const canvas&)=0;
 		virtual std::size_t get_width() const=0;
-        virtual std::size_t get_height() const=0;
+		virtual std::size_t get_height() const=0;
 	};
 	typedef platform_adapter*(*module_enterance)();
 	const char* module_enterance_name="COV_DARWIN_MODULE_MAIN";
