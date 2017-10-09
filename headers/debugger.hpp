8f956979dfd2226c11477392a7d40a9cd54595a2
Michael Lee <mikecovlee@163.com>
2017-10-09 20:02:42 +0800

    更新Darwin

--- headers/debugger.hpp
+++ headers/debugger.hpp
@@ -1,57 +1,72 @@
 #pragma once
+
 #include <cstdio>
 #include <string>
 #include <exception>
 #include "./timer.hpp"
+
 namespace darwin {
 	class outfs final {
-		FILE* mfp=nullptr;
+		FILE *mfp = nullptr;
 	public:
-		outfs()=default;
-		outfs(const char* path):mfp(fopen(path,"w")) {}
-		outfs(const std::string& path):mfp(fopen(path.c_str(),"w")) {}
+		outfs() = default;
+
+		outfs(const char *path) : mfp(fopen(path, "w")) {}
+
+		outfs(const std::string &path) : mfp(fopen(path.c_str(), "w")) {}
+
 		~outfs()
 		{
-			if(this->mfp!=nullptr) fclose(this->mfp);
+			if (this->mfp != nullptr) fclose(this->mfp);
 		}
-		outfs& operator=(const outfs&)=delete;
+
+		outfs &operator=(const outfs &)= delete;
+
 		operator bool() const noexcept
 		{
-			return this->mfp!=nullptr;
+			return this->mfp != nullptr;
 		}
+
 		bool usable() const noexcept
 		{
-			return this->mfp!=nullptr;
+			return this->mfp != nullptr;
 		}
-		void open(const char* path)
+
+		void open(const char *path)
 		{
-			if(this->mfp==nullptr) {
-				this->mfp=fopen(path,"w");
+			if (this->mfp == nullptr) {
+				this->mfp = fopen(path, "w");
 			}
 		}
-		void open(const std::string& path)
+
+		void open(const std::string &path)
 		{
-			if(this->mfp==nullptr) {
-				this->mfp=fopen(path.c_str(),"w");
+			if (this->mfp == nullptr) {
+				this->mfp = fopen(path.c_str(), "w");
 			}
 		}
+
 		void close()
 		{
-			if(this->mfp!=nullptr)
+			if (this->mfp != nullptr)
 				fclose(this->mfp);
-			this->mfp=nullptr;
+			this->mfp = nullptr;
 		}
-		template<typename...ArgsT> void printf(const char* format,ArgsT...args)
+
+		template<typename...ArgsT>
+		void printf(const char *format, ArgsT...args)
 		{
-			fprintf(this->mfp,format,args...);
+			fprintf(this->mfp, format, args...);
 		}
+
 		void flush()
 		{
-			if(this->mfp!=nullptr) {
+			if (this->mfp != nullptr) {
 				fflush(this->mfp);
 			}
 		}
 	};
+
 #ifndef DARWIN_DISABLE_LOG
 	class debugger final {
 		static outfs out;
@@ -68,12 +83,16 @@ namespace darwin {
 #endif
 		static void log(const char* file,int line,const char* func,const char* msg)
 		{
+			if(!out.usable())
+				out.open("./darwin_runtime.log");
 			out.printf("[Darwin Log(%ums)]:In function \"%s\"(%s:%d):%s\n",timer::time(),func,file,line,msg);
 		}
 		static void warning(const char* file,int line,const char* func,const char* msg)
 		{
 			if(ignore)
 				return;
+			if(!out.usable())
+				out.open("./darwin_runtime.log");
 			out.printf("[Darwin Warning(%ums)]:In function \"%s\"(%s:%d):%s\n",timer::time(),func,file,line,msg);
 			if(strict) {
 				out.flush();
@@ -92,7 +111,7 @@ namespace darwin {
 			out.open(path);
 		}
 	};
-	outfs debugger::out("./darwin_runtime.log");
+	outfs debugger::out;
 }
 #define Darwin_Log(msg) darwin::debugger::log(__FILE__,__LINE__,__func__,msg);
 #define Darwin_Warning(msg) darwin::debugger::warning(__FILE__,__LINE__,__func__,msg);
